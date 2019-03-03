f0122e5287ed80c31affe3a74908d40e0daa4661
Comentador <47295402+Comentador@users.noreply.github.com>
2019-03-03 13:01:52 -0800

    Update botip.php

--- botip.php
+++ botip.php
@@ -1,19 +1,52 @@
 <?php
 
 include "curl.php";
-include "parser.php";
 $curl = new Engine();
+$strings = new Strings();
 
 define("BOT_TOKEN", "765733425:AAGoczJFfcw23Uv-tLI7yWhTeh77oxKCKSE");
 define("API_URL", "https://api.telegram.org/bot".BOT_TOKEN."/");
 
-function ler($message){
-	$message_id = $message['message_id'];
-	$chat_id = $message['chat']['id'];
+function process($message){
+		$chat_id=$message['chat']['id'];
+		$message_id=$message["message_id"];
 
-	
+		if(isset($message['text'])){
+			$text = $message['text'];
 
+			if(strpos($text, "/start")){
+				$param = [
+					"chat_id"=>$chat_id, 
+					"text"=> $strings->str->falas['start'].
+					" Para além do seu Ip pode saber sobre outro ip basta informa-lo."];
+				$ip = [
+					"chat_id"=>$chat_id,
+					"text"=>$curl->getDetails()
+				];
 
+				$curl->enviarMensagem('sendMessage', $param);
+				$curl->enviarMensagem('sendMessage', $ip);
 
+			}
+		}else{
+			if($text === "/instruções"){
+				$strings->falas["instruções"];
+			}else if($text ==="/sobre"){
+				$strings->fala["sobre"];
+			}else{
+				$strings->falas["processando"]
+				$curl->remoteIp($text);
+			}
+			
+		}
+
+	}
+
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+
+if(isset($update["message"])){
+	process($update["message"]);
+}
 
 ?>
