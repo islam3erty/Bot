1719046731a18238bb219fcf2032ac4fa6647a19
Comentador <47295402+Comentador@users.noreply.github.com>
2019-03-11 13:08:21 -0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -68,7 +68,7 @@ class Engine {
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($paramm));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($param));
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$resp = curl_exec($ch);
 	curl_close($ch);
