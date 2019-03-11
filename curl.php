e3012976bcd640811105562ffbb38a55f1ec407b
Comentador <47295402+Comentador@users.noreply.github.com>
2019-03-11 13:49:48 -0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -1,8 +1,6 @@
 <?php
 class Engine {
 
-	
-	
 
 	public function __construct(){
 		$this->str = new Strings();
@@ -25,6 +23,25 @@ class Engine {
 		$json = json_decode($output);
 		return $json->wx_desc."<br/>"."Temperatura em Celcius: ".$json->temp_c."°C"."<br/>"."Temperatura em Faranheith: ".$json->temp_f."°F"."<br/>"."Humidade: ".$json->humid_pct."<br/>"."Velocidade do vento(Km/h): ".$json->windspd_kmh."km/h"."<br/>"."Velocidade do vento(m/h): ".$json->windspd_mph."m/h"."<br/>"."Velocidade do vento(m/s): ".$json->windspd_ms."m/s"."<br/>"."Percentagem de Nuvens: ".$json->cloudtotal_pct."%"."<br/>"."Visibilidade: ".$json->vis_km."<br/>"."Pressão do ar: ".$json->slp_mb;
 	}*/
+
+
+	
+
+	public function getDetails(){
+		
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, "http://ipinfo.io/?token=03a2079b5357d1");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$output = curl_exec($ch);
+		$info = curl_getinfo($ch);
+		curl_close($ch);
+
+		$json = json_decode($output);
+		return urlencode("ip: ".$json->ip.PHP_EOL."cidade: ".$json->city."<br/>"."Região: ".$json->region."<br/>"."Pais: ".$json->country."<br/>"."Latitude e Longitude: ".$json->loc."<br/>"."Codigo postal: ".@$json->postal."<br/>"."Servidor da: ".$json->org);
+	}
+
+	
 		
 
 	public function remoteIp($ip){
@@ -40,35 +57,11 @@ class Engine {
 		return "ip: ".$json->ip."<br/>"."cidade: ".$json->city."<br/>"."Região: ".$json->region."<br/>"."Pais: ".$json->country."<br/>"."Latitude e Longitude: ".$json->loc."<br/>"."Codigo postal: ".@$json->postal."<br/>"."Servidor da: ".$json->org;
 	}
 
-	public function bin($bin){
-		$ch = curl_init();
-
-		$url = "https://lookup.binlist.net/".$bin;
-
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept-Version: 3"));
-		$resposta = curl_exec($ch);
-		$info = curl_getinfo($ch);
-		curl_close($ch);
-
-		$json = json_decode($resposta);
-
-
-		return urlencode("Bandeira: ".$json->scheme."\nTipo: ".$json->type."\nBrand: ".$json->brand."\nPaís: ".$json->country->name."(".$json->country->emoji.")"."\nLatitude: ".$json->country->latitude."\nLongitude: ".$json->country->longitude."\nBanco: ".$json->bank->name."\nWebsite: ".$json->bank->url."\nPhone: ".$json->bank->phone."\nCidade: ".$json->bank->city);
-
-	}
-	
 	public function apiRequest($metodo, $param){
-	
+
 	$ch = curl_init();
 
 	curl_setopt($ch, CURLOPT_URL, API_URL.$metodo."?");
-	curl_setopt($ch, CURLOPT_HTTPHEADER,
-            array(
-                "Content-Type" => "multipart/form-data"
-            ));
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
@@ -76,9 +69,22 @@ class Engine {
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$resp = curl_exec($ch);
 	curl_close($ch);
-	return $resp;
 	
-	}
+
+}
+
+	public function env($chat, $msg){
+        $param = [
+            "chat_id" => $chat_id,
+            "disable_web_page_preview" => 1,
+            "parse_mode" => "Markdown",
+            "text" => $msg
+        ];
+        
+        $this->apiRequest("sendMessage", $param);
+
+}
+
 }
 
 
@@ -86,12 +92,17 @@ class Strings
 {
 
 	public $falas = [
-		"start"=>"Eai man. Por enquanto minhas unicas funcões são gerar e checar Bins e INN. Para saber o que é uma bin clica na funcão /acerca. clique no comando a seguir para saber como o que faço. /ferramentas para duvidas e perguntas /sobre. ",
-		"acerca"=>"bin são os primeiros seis números de um cartão do banco que identificam a bandeira do cartão, o tipo, o país, o número de telefone do banco entre outras informações.BIN quer dizer Bank Identification Number.",
-		"erroHTTP"=>"",
+		"start"=>"Bem vindo ao Ipf1nd! Fui programado para localizar e dar informações sobre IP* /instruções*"."<br/>"."* /sobre *",
+		"instruções"=>"Como vê a informação sobre o seu ip foi mostrada quando iniciou uma conversa comigo!. Agora para saber sobre um ip que não seja o seu basta me enviar o Ip","erroVazio"=>"Nenhum ip informado",
+		"erroHTTP"=>"Ip não disponivel para localização",
+		"problemas"=>"Criador: C̶o̶m̶e̶n̶t̶a̶d̶o̶r̶  (https://t.me/Comentered)",
 		"processando"=>"Processando...",
-		"sobre"=>"Criador: Comentador | https://t.me/Comentered. Linguagem: PHP Wsociety@",
-		"ferramentas"=>"Bin-checker:/bin 457173"
+		"sobre"=>"Endereço IP” significa endereço de protocolo de Internet, e cada dispositivo que está conectado a uma rede (como a Internet) possui um.
+
+		Um endereço IP é similar a seu número de telefone. Seu número de telefone é um conjunto único de números que identifica seu telefone, de forma que outras pessoas possam ligar para você. Da mesma forma, um endereço IP é um conjunto único de números que identifica seu computador, de forma que ele possa enviar e receber dados com outros computadores.
+
+		Atualmente, a maioria dos endereços IP consistem em quatro conjuntos de números, cada um separado por um ponto. 192.168.1.42 é um exemplo de endereço IP."
+
 	];
 }
 
