0f26f3a463524851d1309c39b5b8e133733b42e7
Comentador <47295402+Comentador@users.noreply.github.com>
2019-03-12 09:52:37 -0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -1,46 +1,70 @@
 <?php
 class Engine {
-
-
 	public function __construct(){
 		$this->str = new Strings();
 	}
 	//public function getNews(){
 		//$json = file_get_contents("https://newsapi.org/      v2/top-headlines?sources=google-news-br&apiKey=9f8c49a46a4d457082730c4b8d9e2a9a");
-
 	//}
-
-
-
-	/*public function getTemperatura(){
-		$ch = curl_init();
-
-		curl_setopt($ch, CURLOPT_URL, "http://api.weatherunlocked.com/api/current/-25.9653,32.5892?app_id=66fa65c0&app_key=409b1bb29c9bd5643b0744fd5fc15b03");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$output = curl_exec($ch);
-		$info = curl_getinfo($ch);
-		curl_close($ch);
-		$json = json_decode($output);
-		return $json->wx_desc."<br/>"."Temperatura em Celcius: ".$json->temp_c."°C"."<br/>"."Temperatura em Faranheith: ".$json->temp_f."°F"."<br/>"."Humidade: ".$json->humid_pct."<br/>"."Velocidade do vento(Km/h): ".$json->windspd_kmh."km/h"."<br/>"."Velocidade do vento(m/h): ".$json->windspd_mph."m/h"."<br/>"."Velocidade do vento(m/s): ".$json->windspd_ms."m/s"."<br/>"."Percentagem de Nuvens: ".$json->cloudtotal_pct."%"."<br/>"."Visibilidade: ".$json->vis_km."<br/>"."Pressão do ar: ".$json->slp_mb;
-	}*/
-
+	
 	public function remoteIp($ip){
 		$ch = curl_init();
-
 		curl_setopt($ch, CURLOPT_URL, "http://ipinfo.io/".$ip."?token=03a2079b5357d1");
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		$output = curl_exec($ch);
 		$info = curl_getinfo($ch);
 		curl_close($ch);
-
 		$json = json_decode($output);
 		return "ip: ".$json->ip."\ncidade: ".$json->city."\nRegião: ".$json->region."\nPais: ".$json->country."\nLatitude e Longitude: ".$json->loc."\nCodigo postal: ".@$json->postal."\nServidor da: ".$json->org;
 	}
 
+	public function genCC($tipo){
+		
+		$ch= curl_init();
+		$url = "https://api.bincodes.com/cc-gen/json/e09351d196aa31f07d053f3571fef571/".$tipo."/";
+
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$resposta = curl_exec($ch);
+		$info = curl_getinfo($ch);
+		curl_close($ch);
+
+		$json = json_decode($resposta);
+
+		return $json->card."\n".$json->number;
+	}
+	//Tive que repetir esse metodo porque n achei uma logica para fazer atraves do genCC...
+	public function binGen(){
+
+		$bandeiras=[
+			"mastercard",
+			"visa",
+			"amex",
+			"jcb",
+			"diners",
+			"maestro"
+		];
+		$chose = array_rand($bandeiras, 1);
+		
+		$ch= curl_init();
+		$url = "https://api.bincodes.com/cc-gen/json/e09351d196aa31f07d053f3571fef571/".$bandeiras[$chose]."/";
+
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$resposta = curl_exec($ch);
+		$info = curl_getinfo($ch);
+		curl_close($ch);
+
+		$json = json_decode($resposta);
+
+		return "BIN".$json->card."\n".substr($json->number, 0,6);
+	}
+
+
 	public function apiRequest($metodo, $param){
-
 	$ch = curl_init();
-
 	curl_setopt($ch, CURLOPT_URL, API_URL.$metodo."?");
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -50,9 +74,7 @@ class Engine {
 	$resp = curl_exec($ch);
 	curl_close($ch);
 	
-
 }
-
 	public function env($opc, $msg){
         $param = [
             "chat_id" => $opc["chat_id"],
@@ -62,13 +84,10 @@ class Engine {
         ];
         
         $this->apiRequest("sendMessage", $param);
-
 }
 	public function bin($bin){
 	$ch = curl_init();
-
 	$url = "https://lookup.binlist.net/".$bin;
-
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -76,34 +95,19 @@ class Engine {
 	$resposta = curl_exec($ch);
 	$info = curl_getinfo($ch);
 	curl_close($ch);
-
 	$json = json_decode($resposta);
-
-
 	return "Bandeira: ".$json->scheme."\nTipo: ".$json->type."\nBrand: ".$json->brand."\nPaís: ".$json->country->name."(".$json->country->emoji.")"."\nLatitude: ".$json->country->latitude."\nLongitude: ".$json->country->longitude."\nBanco: ".$json->bank->name."\nWebsite: ".$json->bank->url."\nPhone: ".$json->bank->phone."\nCidade: ".$json->bank->city;
 }
-
 }
-
-
 class Strings
 {
-
 	public $falas = [
-		"start"=>"Eai mano você ta querendo checar sua bin?. ta no lugar certo. clique no comando /ferramentas para saber todas minhas funcionalidades.\nSe ainda n sabe o que é uma BIN clique no comando /acerca.\n Tem Duvidas? clique no comando /sobre.",
-		"acerca"=>"bin são os primeiros seis números de um cartão do banco que identificam a bandeira do cartão, o tipo, o país, o número de telefone do banco entre outras informações.BIN quer dizer Bank Identification Number.",
-		"sobre"=>"Criador: ̶C̶o̶m̶e̶n̶t̶a̶d̶o̶r̶ | https://t.me/Comentered.\nLinguagem: PHP Wsociety@",
-		"ferramentas"=>"Ferramentas:\n/bin 404528\n\n/gen"
+		"start"=>"Sou programado para fazer varias coisas legais. clique no comando /ferramentas para saber todas minhas funcionalidades e introduza os comandos de acordo como está exemplificado.\nSe ainda n sabe o que é uma BIN clique no comando /acerca.\n Tem Duvidas? clique no comando /sobre.",
+		"acerca"=>"bin são os primeiros seis números de um cartão do banco que identificam a bandeira do cartão, o tipo, o país, o número de telefone do banco entre outras informações.BIN quer dizer Bank Identification Number.\n\nUm Endereço de Protocolo da Internet (Endereço IP), do inglês Internet Protocol address (IP address), é um rótulo numérico atribuído a cada dispositivo (computador, impressora, smartphone etc.) conectado a uma rede de computadores que utiliza o Protocolo de Internet para comunicação.[1] Um endereço IP serve a duas funções principais: identificação de interface de hospedeiro ou de rede e endereçamento de localização ex: 159.89.157.64.",
+		"sobre"=>"Criador: ̶C̶o̶m̶e̶n̶t̶a̶d̶o̶r̶ | https://t.me/Comentered.\n\nLinguagem: PHP Wsociety@",
+		"ferramentas"=>"Ferramentas:\nChecar Bin: /bin 404528\n\nGerar Cartão de Credito: /CCGen\n\nGerar Bin: /BinGen\n\nGeolocalizar ip: /ip 159.89.157.64",
+		"bandeiras"=>"/MasterCard\n\n/Visa\n\n/Maestro\n\n/Amex\n\n/Jcb/\n\n/Diners"
 
 	];
 }
-
-
-
-
-
-
-
-
-
 ?>
