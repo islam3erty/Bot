fd93c763c75fecf23fe8b27cb1e3f446c5187f87
Comentador <47295402+Comentador@users.noreply.github.com>
2019-03-12 09:51:54 -0700

    Update botip.php

--- botip.php
+++ botip.php
@@ -1,23 +1,15 @@
 <?php
-
-
 require "curl.php";
 $motor = new Engine();
 $strings = new Strings();
-
-
 define("BOT_TOKEN", "765733425:AAGoczJFfcw23Uv-tLI7yWhTeh77oxKCKSE");
 define("API_URL", "https://api.telegram.org/bot".BOT_TOKEN."/");
-
 $conteudo = file_get_contents("php://input");
 $update = json_decode($conteudo, TRUE);
 $mensagem = $update["message"];
 $opc = [];
 $opc["chat_id"]=$mensagem["chat"]["id"];
 $opc["texto"] = $mensagem["text"];
-
-
-
 //Metodo Get pra quem quiser simplicidade. Mais n faz quebra de linhas. by C̶o̶m̶e̶n̶t̶a̶d̶o̶r̶ 
 /*if ($texto === "/start"){
 	file_get_contents(API_URL."sendmessage?chat_id=".$chat_id."&text=".$strings->falas["start"]);
@@ -33,7 +25,6 @@ $opc["texto"] = $mensagem["text"];
 }else{
 	file_get_contents(API_URL."sendmessage?chat_id=".$chat_id."&text=Função desconhecida");
 }*/
-
 if ($opc["texto"] === "/start"){
 	
 	$motor->env($opc, $strings->falas["start"]);
@@ -50,16 +41,35 @@ if ($opc["texto"] === "/start"){
 }else if($opc["texto"] === "/sobre"){
 	
 	$motor->env($opc, $strings->falas["sobre"]);
+
 }else if($opc["texto"] === "/ferramentas"){
 	
 	$motor->env($opc, $strings->falas["ferramentas"]);
-}else{
+
+}else if($opc["texto"] === "/CCGen"){
 	
+	$motor->env($opc, $strings->falas["bandeiras"]);
+
+}else if($opc["texto"] === "/MasterCard"){
+	$motor->env($opc, $motor->ccGen("mastercard"));
+
+}else if($opc["texto"] === "/Visa"){
+	$motor->env($opc, $motor->ccGen("visa"));
+
+}else if($opc["texto"] === "/Amex"){
+	$motor->env($opc, $motor->ccGen("amex"));
+
+}else if($opc["texto"] === "/Diners"){
+	$motor->env($opc, $motor->ccGen("diners"));
+
+}else if($opc["texto"] === "/Maestro"){
+	$motor->env($opc, $motor->ccGen("maestro"));
+
+}else if($opc["texto"] === "/Jcb"){
+	$motor->env($opc, $motor->ccGen("jcb"));
+}else if($opc["texto"] === "/BinGen"){
+	$motor->env($opc, $motor->binGen());
+}else{
 	$motor->env($opc, "Função desconhecida");
 }
-
-
-
-
-
 ?>
