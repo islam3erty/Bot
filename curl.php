7a3ff34ad6634738c25d4935e8e88112710a6c8a
Comentador <bcomentador@gmail.com>
2019-03-21 12:49:13 -0700

    vamo que vamo

--- curl.php
+++ curl.php
@@ -102,7 +102,7 @@ class Engine {
 		$data = [
 			'chat_id' => $opc["chat_id"],
 			'text' => $msg,
-			'reply_markup' => ['inline_keyboard' => $inline_keyboard
+			"reply_markup" => array("inline_keyboard" => $this->str->falas["inlineBTMedia"])
 		]];
     		
 
@@ -133,6 +133,7 @@ class Strings
 		"sobre"=>"Criador: ̶C̶o̶m̶e̶n̶t̶a̶d̶o̶r̶ | https://t.me/Comentered.\n\nLinguagem: PHP Wsociety@",
 		"ferramentas"=>"_Ferramentas_:\n*Checar Bin:* `/bin 404528`\n*Gerar Cartão de Credito:* `/ccgen`\n*Gerar Bin:* `/bgen`\n*Geolocalizar ip:* `/ip 159.89.157.64`",
 		"bandeiras"=>"*Escolha a bandeira da cc que deseja gerar:* \n`/mastercard\n/visa\n/amex\n/jcb\n/diners\n/maestro`",
+		"inlineBTMedia" => [[["text" => "\u{1F4C3} Mais Informações", "callback_data" => "media_id"]]],
 		"sintaxes"=>"Formato incoreto. Insira o comando no seguinte formato:\n\n/bin xxxxxx\n\n em que:\n\n/bin é o comando\n\n xxxxxx são os 6 números da bin que deseja checar"
 		
 
