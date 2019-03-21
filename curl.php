abc1d81a0ce6e6d86c2acefd108d8aef47953376
Comentador <47295402+Comentador@users.noreply.github.com>
2019-03-21 15:21:25 -0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -92,17 +92,21 @@ class Engine {
 	}
 
 	public function keyboard($opc, $msg){
-		
+		$data["reply_markup"]=[
+			"inline_keyboard"=>[
+				[
+					[
+						"text"=>"Boton",
+						"callback_data"=>"botao_id"
+					],	
+				],
+			],
+		];
 		
 		$data = [
 			'chat_id' => $opc["chat_id"],
 			'text' => $msg,
-			'reply_markup' => array("inline_keyboard" => array(
-				array(
-					array("text"=>"Seu botao", "callback_data"=>"botao_id"),
-				),
-			),
-		),
+			
 		];
     		
 
