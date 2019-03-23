3c3333ff9347e2ae108a87304d11335dff609bf8
Comentador <bcomentador@gmail.com>
2019-03-23 03:29:59 -0700

    ohh

--- curl.php
+++ curl.php
@@ -147,7 +147,7 @@ class Engine {
 			];
 
 			if($cb_data == "Visa"){
-				$text = $this->env($opc, "Eu sou foda");
+				$text ="Eu sou foda";
 
 				$this->answercallback($cb_id, false, 3,  $text);
 			}
