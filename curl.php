e3c3b45a631ebad4e518bcd4d79a291974b6031b
Comentador <bcomentador@gmail.com>
2019-03-23 01:53:16 -0700

    track

--- curl.php
+++ curl.php
@@ -148,7 +148,7 @@ class Engine {
 			if($cb_data == "Visa"){
 				$text = "/visa";
 
-				$this->answercallback($cb_id, false, 3, $text);
+				$this->answercallback($cb_id, true, 3, $text);
 			}
 	}
 
