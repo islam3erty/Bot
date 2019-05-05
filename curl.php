bb2462a884901610fce2189ae04c443589f27969
Comentador <bcomentador@gmail.com>
2019-05-05 06:32:07 -0700

    bora

--- curl.php
+++ curl.php
@@ -135,19 +135,18 @@ class Engine {
 		$this->apiRequest("answerCallbackQuery", $param);
 	}
 
-	public function callback($callback){
+	public function callback($opc ,$callback){
 		
 			$cb_chat_id = $callback["message"]["chat"]["id"];
 			$cb_message_id = $callback["message"]["id"];
 			$cb_id = $callback["id"];
 			$cb_data = $callback["data"];
 
-			$opc = [
+			/*$opc = [
 				"chat_id"=>$cb_chat_id,
-				"msg_id"=>$cb_message_id
+				"msg_id"=>$cb_message_id,
 
-
-			];
+			];*/
 
 			$bandeiras = [
 					"amex"=>1,
