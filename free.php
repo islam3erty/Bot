65370f4470aa911b485de043eba493fbdae6c0a6
Comentador <47295402+Comentador@users.noreply.github.com>
2020-04-29 14:30:37 -0700

    Add files via upload

---
+++ free.php
@@ -0,0 +1,39 @@
+<?php
+
+	require "freeClass.php";
+
+	define("BOT_TOKEN", "833445680:AAGjpwc2TMP2RMXv0G04meBpdluL-qRmKsU");
+	define("API_URL", "https://api.telegram.org/bot".BOT_TOKEN."/");
+	define("WEBHOOK_URL", "https://botip.herokuapp.com/free.php");
+
+	$receive = file_get_contents("php//input");
+	$decode = json_decode($receive, true);
+	$mensagem = $decode["message"];
+
+	$opc["chat_id"]=""; //pendente;
+	$opc["texto"] = $mensagem['text'];
+	$opc["message_id"] = $mensagem["message_id"]+2;
+	$opc["reply_markup"] = $mensagem["reply_markup"];
+
+	$start = new Bot();
+	$buttons = new Strings();
+
+	$final_markup = array_push($opc["reply_markup"]["inline_keyboard"], $buttons->falas["contact"]);
+
+	if(isset($decode["callback_query"])){
+		$start->callback($update["callback_query"]);
+	}
+
+	switch ($opc["texto"]) {
+		case 'start':
+			$start->sendMessage($opc, "Eai Viado Como vai?", $buttons->falas["inline"]);
+			break;
+		
+		case "edit":
+			$start->editReply_Markup($opc, $final_markup);
+			break;
+		default:
+			sleep(1);
+			break;
+	}
+?>
---
+++ freeClass.php
@@ -0,0 +1,103 @@
+<?php
+
+
+class Bot {
+
+	public $str;
+	private $ch;
+	function __construct(){
+		$this->str = new Strings();
+		$this->ch = curl_init();
+	}
+
+	public function api_Request($metodo, $parametro){
+
+		curl_setopt($this->ch, CURLOPT_URL, API_URL.$metodo."?");
+		curl_setopt($this->ch, CURLOPT_HTTPHEADER, [
+			"Content_Type" => "multipart/form-data"
+		]);
+		curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+		$resultado = curl_exec($this->ch);
+		$info = curl_getinfo($this->ch);
+		curl_close($this->ch);
+	}
+
+	public function editMessage($opc, $msg, $button=null){
+
+
+		if($button == null){
+			$parametro = [
+				"chat_id"=>$opc["chat-id"],
+				"parse_mode"=>"Markdown",
+				"message_id"=>$opc["message_id"],
+				"text"=>$msg,
+			];
+		}else{
+			$button = json_encode($button, true);
+			$parametro = [
+				"chat_id"=>$opc["chat-id"],
+				"parse_mode"=>"Markdown",
+				"message_id"=>$opc["message_id"],
+				"text"=>$msg,
+				"reply_markup"=>$button,
+			];
+		}
+
+		$this->api_Request("editMessageText", $parametro);
+	}
+
+
+	public function callbackQuery($callback){
+		
+		$cb_chat_id = $callback["message"]["chat"]["id"];
+		$cb_message_id = $callback["message"]["id"];
+		$cb_id = $callback["id"];
+		$cb_data = $callback["data"];
+
+		$opc = [
+			"chat_id"=>$cb_chat_id,
+		];
+	}
+
+	public function answerCallbackQuery($callback_id, $alert, $time, $text){
+		$parametro = [
+			"callback_query_id"=>$callback_id,
+			"show_alert"=>$alert,
+			"cache_time"=>$time,
+			"text"=>$text
+		];
+
+		$this->api_Request("answerCallbackQuery", $parametro);
+	}
+
+	public function editReply_Markup($opc, $button){
+
+
+		$parametro = [
+			"chat_id"=>$opc["chat_id"],
+			"message_id"=>$opc["message_id"],
+			"reply_markup"=>json_encode($button, true),
+		];
+
+		$this->api_Request("editMessageReplyMarkup", $parametro);
+	}
+}
+
+
+class Strings{
+
+	public $falas = [
+		"contact" => array(array("text"=>"Contact Me", "url"=>"t.me/Comentered")),
+		"inline" => array(
+			"inline_keyboard"=>array(
+				array(array("text"=>"Like", "callback"=>"like")),
+			)
+		)
+	];
+}
+
+
+
+?>
