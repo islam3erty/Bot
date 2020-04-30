f230aa0d02c838cf477bf304910ab07ab0170562
Comentador <47295402+Comentador@users.noreply.github.com>
2020-04-30 07:35:20 -0700

    Add files via upload

--- free.php
+++ free.php
@@ -1,5 +1,6 @@
 <?php
-
+	
+	sleep(4);
 	require "freeClass.php";
 
 	define("BOT_TOKEN", "833445680:AAGjpwc2TMP2RMXv0G04meBpdluL-qRmKsU");
@@ -10,33 +11,21 @@
 	$decode = json_decode($receive, true);
 	$mensagem = $decode["message"];
 
-	$opc["chat_id"]=600217408;
-	$opc["texto"] = $mensagem['text'];
-	$opc["message_id"] = $mensagem["message_id"]+2;
+	$opc["chat_id"]="@latitudeDell";
+	$opc["texto"] = "start";//$mensagem['text'];
+	$opc["message_id"] = $mensagem["message_id"];
 	$opc["reply_markup"] = $mensagem["reply_markup"];
 
 	$start = new Bot();
 	$buttons = new Strings();
 
-	$final_markup = array_push($opc["reply_markup"]["inline_keyboard"], $buttons->falas["contact"]);
+	//$final_markup = array_push($opc["reply_markup"]["inline_keyboard"], $buttons->falas["contact"]);
 
 	if(isset($decode["callback_query"])){
 		$start->callback($update["callback_query"]);
 	}
 
-	switch ($opc["texto"]) {
-		case 'start':
-			$start->sendMessage($opc, "Eai Viado Como vai?", $buttons->falas["inline"]);
-			sleep(2);
-			$start->editReply_Markup($opc, $final_markup);
-			break;
+	$start->editMessage($opc, "Mudei a porra toda vei", $buttons->falas["inline"]);
 		
-		case "edit":
-			$start->editReply_Markup($opc, $final_markup);
-			break;
-		default:
-			sleep(1);
-			break;
-	}
 
 ?>
