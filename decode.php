6af4d56ac70c2f56f0f8d69f2850961a00e2f8c4
Comentador <47295402+Comentador@users.noreply.github.com>
2020-05-01 05:20:55 -0700

    Update decode.php

--- decode.php
+++ decode.php
@@ -1,9 +1,10 @@
 <?php
-	/*$name = md5("text").".txt";
+	$name = md5("text").".txt";
 
 	$fp = fopen($name, "w+");
 	@fwrite($fp, "Contact Me");
-	fclose($fp);*/
-	echo file_get_contents(md5("text").".txt");
+	fclose($fp);
+	echo "Complete";
+	//echo file_get_contents(md5("text").".txt");
 	
 ?>
